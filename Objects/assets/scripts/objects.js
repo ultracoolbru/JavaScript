daebf2b7a2f78deb53b946812a3e6f3e28d652d4
Edward Whitehead <edward@ultracoolbru.com>
2022-11-14 22:17:50 +0300

    Next up classes and OOP

--- Objects/assets/scripts/objects.js
+++ Objects/assets/scripts/objects.js
@@ -148,7 +148,7 @@ const searchBtn = document.getElementById('search-btn');
 
 let movies = [];
 
-const renderMovie = (movie = '') => {
+const renderMovies = (movie = '') => {
     const movieList = document.getElementById('movie-list');
     if (movies.length === 0) {
         movieList.classList.remove('visible');
@@ -157,30 +157,92 @@ const renderMovie = (movie = '') => {
         movieList.classList.add('visible');
     }
 
-    const filterMovie = !movie ? movies.filter(movie => movie.info.title.includes(movie)) : movie;
+    const filteredMovies = !movie ? movies : movies.filter((movieItem) => movieItem.info.title.includes(movie));
 
-    // Not good practice to clear the list and re-render it.
-    // movieList.innerHTML = '';
-    // movie.forEach((movie) => {
-    //     const movieElement = document.createElement('li');
-    //     movieElement.textContent = movie.info.title;
-    //     movieList.append(movieElement);
-    // });
+    // I do not like this solution, but it works instead of looping through all objects again and then rendering them again.
+    movieList.innerHTML = '';
 
-    // This is a better way to render the list.
-    const movieElement = document.createElement('li');
-    let text = filterMovie.info.title + ' - ';
+    if (movie !== '') {
+        filteredMovies.forEach(movie => {            
+            const movieElement = document.createElement('li');
 
-    // Dynamic property access loop.
-    // This allows us to access any property of the movie object and display it, when we do not know it in advance.
-    for (const key in filterMovie.info) {
-        if (key !== 'title') {
-            text = text + `${key}: ${filterMovie.info[key]}`;
-        }
+            // Object destructuring.
+            // Object destructing basically extracts the values from an object and stores them in variables.
+            // It is important to note that the variable names must match the property names. 
+            // If you want to use a different name, you can use the following syntax:
+            // const { title: movieTitle } = movie.info;
+            // If you want to extract other properties, you can use the following syntax:
+            const { info, ...otherProperties } = movie;
+            console.log(otherProperties);
+
+            // Object destructuring on destructed object.
+            const { title: movieTitle } = info;
+
+            let text = movieTitle + ' - ';
+
+            for (const key in info) {
+                if (key !== 'title') {
+                    text = text + `${key}: ${info[key]}`;
+                }
+            }
+            
+            movieElement.textContent = text;
+            movieList.append(movieElement);
+        });
+        movie = '';
+    } else {
+        movies.forEach(movie => {
+            const movieElement = document.createElement('li');
+
+            // Checking for property existence.
+            if ('info' in movie) {
+                console.log(movie.info.title);
+            }
+            if (movie.info === undefined) { 
+                return;
+            }
+
+            // Object destructuring.
+            // Object destructing basically extracts the values from an object and stores them in variables.
+            // It is important to note that the variable names must match the property names. 
+            // If you want to use a different name, you can use the following syntax:
+            // const { title: movieTitle } = movie.info;
+            // If you want to extract other properties, you can use the following syntax:
+            const { info, ...otherProperties } = movie;
+            console.log(otherProperties);
+
+            // Object destructuring on destructed object.
+            const { title: movieTitle } = info;
+
+            const { getFormattedTitle } = movie;
+            const { getFormattedExtraName } = movie;
+            // const { getFormattedExtraValue } = movie;   // NOT WORKING AS EXPECTED.
+
+            console.log(getFormattedTitle.call(movie));
+
+            // let text = movieTitle + ' - ';
+            let text = getFormattedTitle.call(movie) + ' - ';
+
+            // You can also use the function call to get access to unknown properties.
+            let extraName = getFormattedExtraName.call(movie);
+
+            // You can use 'apply' to get the function to execute and pass in an array of arguments.
+            // Unlike call, you pass a comma separated list of arguments.
+            // let extraValue = getFormattedExtraValue.apply(movie, ['director']);  // NOT WORKING AS EXPECTED.
+
+            for (const key in info) {
+                if (key !== 'title' &&
+                // The _title refers to the getter/setter property below and also prevents the property from being rendered.
+                key !== '_title') {
+                    text = text + `${extraName}: ${info[key]}`;
+                    // text = text + `${extraName}: ${extraValue}`;
+                }
+            }
+
+            movieElement.textContent = text;
+            movieList.append(movieElement);
+        });
     }
-
-    movieElement.textContent = text;
-    movieList.append(movieElement);
 };
 
 const clearFields = () => {
@@ -198,30 +260,107 @@ const addMovieHandler = () => {
     const extraName = document.getElementById('extra-name').value;
     const extraValue = document.getElementById('extra-value').value;
 
-    if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+    if (extraName.trim() === '' || extraValue.trim() === '') {
         return;
     }
 
     const newMovie = {
         info: {
             // If the key and value are the same, you can use the shorthand syntax.
-            title,
+            get title() {
+                return this._title;
+            },
+            set title(value) {
+                if (value.trim() === '') {
+                    this._title = 'DEFAULT';
+                    return;
+                }
+                this._title = value;
+            },
             // Dynamic property name and value.
             [extraName]: extraValue
         },
-        id: Math.random()
+        id: Math.random(),
+        // The 'this' keyword
+        // Do not use the arrow function here, because the function will not have access to the 'this' keyword.
+        getFormattedTitle: function () {
+            return this.info.title.toUpperCase();
+        },
+        getFormattedExtraName() {
+            return this.info[extraName].toUpperCase();
+        },
+        // NOT WORKING AS EXPECTED.
+        // You can also pass arguments to the function.
+        getExtraValue(director) {
+            console.log(director); // WORKS
+            console.log(this.info[extraValue]); // WORKS
+            return this.info[extraValue]; // DOES NOT WORK RETURNING UNDEFINED
+        }
     };
+
+    // You assign/set the getter/setter function by using the following syntax.
+    newMovie.info.title = title;
+    console.log(newMovie.info.title);
 
     movies.push(newMovie);
 
-    renderMovie(newMovie);
+    renderMovies();
     clearFields();
 };
 
+// When you use the arrow function, the 'this' keyword will not work. It will instead refer to the global object.
 const searchMovieHandler = () => {
+    console.log(this);  // This will write 'Window' to the console.
+
     const filterTerm = document.getElementById('filter-title').value;
-    renderMovie(filterTerm);
+    // const newMoviewFilter = {
+    //     info: {
+    //         title: filterTerm
+    //     }
+    // };
+    renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
 searchBtn.addEventListener('click', searchMovieHandler);
+
+// Example of the spread operator.
+const personSpreadOperator = {
+    name: 'Edward',
+    age: 49,
+    hobbies: ['music', 'movies', 'games']
+};
+
+personSpreadOperator.hobbies.push('coding');
+
+// This is the spread operator. What is does is make a copy of the person object.
+// What is important to note is that it does not create a deep copy, but a shallow copy.
+// This means that if you have an array inside of an object, it will not create a copy of the array, but a reference to the array.
+const copiedPerson = { ...personSpreadOperator };
+
+console.log(copiedPerson);
+
+// The age will be overwritten in the copiedPerson object.
+// Using the spread operator, we can make a clone of an array from an object.
+const personCopies = { ...copiedPerson, age: 50, hobbies: [...copiedPerson.hobbies] };
+console.log(personCopies);
+
+// Object.assign() is another way to copy an object.
+// Copy the values of all of the enumerable own properties from one or more source objects to a target object. 
+// Returns the target object.
+const nameofPerson = { name: 'Edward', hobbies: ['music', 'movies', 'games'] };
+const assignOfPerson = Object.assign({}, nameofPerson);
+console.log(assignOfPerson);
+
+// Arrow functions within objects.
+const member = {
+    team: 'Manchester United',
+    members: ['Ronaldo', 'Messi', 'Neymar'],
+    getTeamWithMembers() {
+        console.log(this.members.map(member => `${member} is on the ${this.team} team.`));
+        this.members.forEach(member => {
+            console.log(`${member} is on the ${this.team}`);
+        });
+    }   
+};
+member.getTeamWithMembers();
